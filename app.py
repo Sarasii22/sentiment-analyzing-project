fd91f80cfaa9570a20cf5d514837efc9554862f3
Sarasi <eg245387@engug.ruh.ac.lk>
2026-03-11 20:37:49 +0530

    fix overfitting

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
+#import functions from helper.py for preprocessing, vectorization and prediction 
+from helper import preprocessing, vectorizer, get_prediction
+from logger import logging
+
 app = Flask(__name__)
 
-data = dict()
-reviews = ['Great product!', 'Not bad, could be better.', 'I love it!', 'Bad quality, do not recommend.', 'Not worth the price.']
-positive = 3
-negative = 2
+logging.info('Flask server started')
 
-@app.route('/')
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/")
 def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('========== Open home page ============')
+
     return render_template('index.html', data=data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/", methods = ['post'])
+def my_post():
+    text = request.form['text']
+    logging.info(f'Text : {text}')
+
+    preprocessed_txt = preprocessing(text)
+    logging.info(f'Preprocessed Text : {preprocessed_txt}')
+
+    vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'Vectorized Text : {vectorized_txt}')
+
+    prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction : {prediction}')
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+    
+    reviews.insert(0, text)
+    #refres the page after post request
+    return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run()
